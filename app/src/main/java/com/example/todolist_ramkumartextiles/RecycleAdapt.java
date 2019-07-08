1536f4c7258f26cb943ca6a4c9a435a1c1d2dd59
Kalit31 <inanikalit31@gmail.com>
2019-07-08 22:56:02 +0530

    Implementation: - Save tasks by owners - Employee retrieving their tasks.

---
+++ app/src/main/java/com/example/todolist_ramkumartextiles/RecycleAdapt.java
@@ -0,0 +1,54 @@
+package com.example.todolist_ramkumartextiles;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecycleAdapt extends RecyclerView.Adapter<RecycleAdapt.ViewHolder>
+{
+
+    private ArrayList<TaskInformation> items;
+
+
+    public RecycleAdapt(ArrayList<TaskInformation> items)
+    {
+        this.items = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.task_layout,viewGroup,false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder( final ViewHolder viewHolder, int i) {
+        viewHolder.task.setText(items.get(i).getDesc());
+        viewHolder.date.setText(items.get(i).getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView task, date;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            task = itemView.findViewById(R.id.tV_task);
+            date = itemView.findViewById(R.id.tV_date);
+        }
+    }
+}
--- app/src/main/java/com/example/todolist_ramkumartextiles/ToDoActivity.kt
+++ app/src/main/java/com/example/todolist_ramkumartextiles/ToDoActivity.kt
@@ -1,21 +1,32 @@
 package com.example.todolist_ramkumartextiles
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_to_do.*
 
 class ToDoActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private var items = ArrayList<TaskInformation>()
+    private lateinit var adapter : RecycleAdapt
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
 
         auth = FirebaseAuth.getInstance()
+
+        databaseReference = FirebaseDatabase.getInstance().getReference()
 
         if(auth.currentUser == null)
         {
@@ -24,12 +35,42 @@ class ToDoActivity : AppCompatActivity() {
         }
         val user = auth.currentUser
         if (user != null) {
-            textView.text = "Welcome" + user.email
+            textView.text = "Welcome  " + user.email
         }
         logout.setOnClickListener {
             auth.signOut()
             finish()
             startActivity(Intent(applicationContext, LoginActivity::class.java))
         }
+        databaseReference.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (ds in dataSnapshot.children)
+                {
+                    val item: TaskInformation? = ds.getValue(TaskInformation::class.java)
+                    if(item!!.getEmployeeName() == user!!.email)
+                    {
+                        items.add(item)
+                    }
+                }
+                val it: TaskInformation? = TaskInformation("inanikalit31@gmail.com","Make payments","10/07/19")
+                items.add(it!!)
+                Toast.makeText(applicationContext,"Reached here", Toast.LENGTH_SHORT).show()
+                adapter = RecycleAdapt(items)
+                recyclerView.layoutManager = LinearLayoutManager(this@ToDoActivity)
+                //recyclerView.hasFixedSize() = true
+                recyclerView.adapter= adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+            //    Log.w(TAG, "Failed to read value.", error.toException())
+                Toast.makeText(applicationContext,error.toString(),Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
+
