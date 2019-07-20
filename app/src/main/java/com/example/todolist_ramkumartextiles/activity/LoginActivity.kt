7d1f049a065cb85c16e2b0ca87bd57a7a812eb07
Kalit31 <inanikalit31@gmail.com>
2019-07-20 21:21:17 +0530

    Fixed login bug.

--- app/src/main/java/com/example/todolist_ramkumartextiles/activity/LoginActivity.kt
+++ app/src/main/java/com/example/todolist_ramkumartextiles/activity/LoginActivity.kt
@@ -10,7 +10,13 @@ import android.text.TextUtils
 import android.widget.Toast
 import com.example.todolist_ramkumartextiles.R
 import com.example.todolist_ramkumartextiles.services.LocationService
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -18,6 +24,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private var username = null
     private lateinit var sharedPreferences:SharedPreferences
+    private var userList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,17 +44,34 @@ class LoginActivity : AppCompatActivity() {
 
 
         login.setOnClickListener {
-            val username = etUsernameL.text.toString()
-            val email = emailL.text.toString()
-            val password = etPasswordL.text.toString()
+            var username = etUsernameL.text.toString()
+            var email = emailL.text.toString()
+            var password = etPasswordL.text.toString()
             if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty((username))) {
                 Toast.makeText(applicationContext, "Please enter all details", Toast.LENGTH_SHORT).show()
             } else {
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
+                readUsers(object: FirebaseCallback
+                {
+                    override fun onCallback(list: ArrayList<String>) {
+                        if(list.contains(username))
+                        {
+                            val ref = FirebaseDatabase.getInstance().getReference("Users")
+                                .child(username).child("email")
+                            ref.addValueEventListener(object: ValueEventListener
+                            {
+                                override fun onCancelled(p0: DatabaseError) {
+                                        Toast.makeText(applicationContext, p0.toString(),Toast.LENGTH_SHORT).show()
+                                }
+
+                                override fun onDataChange(p0: DataSnapshot) {
+                                    val test_email = p0.getValue(String::class.java)
+                                    if(test_email!!.compareTo(email) == 0)
+                                    {
+                                            auth.signInWithEmailAndPassword(email,password).
+                                                    addOnCompleteListener(this@LoginActivity) {task ->
+                                                                                if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
-                            finish()
+                                                                                     finish()
                             val edit: SharedPreferences.Editor = sharedPreferences.edit()
                             edit.putString("username", username)
                             edit.putBoolean("LoginStatus", true)
@@ -60,10 +84,76 @@ class LoginActivity : AppCompatActivity() {
                             Toast.makeText(
                                 baseContext, "Login failed.",
                                 Toast.LENGTH_SHORT
+                               ).show()
+                           }
+                         }
+                     }
+                                    else
+                                        Toast.makeText(
+                                            baseContext, "Invalid email",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+
+                                }
+                            })
+                        }
+                        else {
+                            Toast.makeText(
+                                baseContext, "Invalid username",
+                                Toast.LENGTH_SHORT
                             ).show()
                         }
                     }
+
+                })
+
+//                auth.signInWithEmailAndPassword(email, password)
+//                    .addOnCompleteListener(this) { task ->
+//                        if (task.isSuccessful) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            finish()
+//                            val edit: SharedPreferences.Editor = sharedPreferences.edit()
+//                            edit.putString("username", username)
+//                            edit.putBoolean("LoginStatus", true)
+//                            edit.apply()
+//                            val todDoIntent = Intent(applicationContext, ToDoActivity::class.java)
+//                            todDoIntent.putExtra("username", username)
+//                            startActivity(todDoIntent)
+//
+//                        } else {
+//                            Toast.makeText(
+//                                baseContext, "Login failed.",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//                    }
             }
         }
     }
+
+    private fun readUsers(firebaseCallback: FirebaseCallback) {
+        var ref = FirebaseDatabase.getInstance().getReference("Usernames")
+        ref.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                userList.clear()
+                for (ds in dataSnapshot.children) {
+                    val user = ds.getValue(String::class.java)
+                    if (user != null) {
+                        userList.add(user)
+                    }
+                }
+                firebaseCallback.onCallback(userList)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+    private interface FirebaseCallback{
+        fun onCallback(list:ArrayList<String>)
+    }
+
 }
